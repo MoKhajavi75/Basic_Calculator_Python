4351eb6dcf45044ea22c2094e7ca5dde33e556ef
Mohamad Khajavi <khajavi_mohamad@yahoo.com>
2017-05-28 09:49:14 +0430

    testing

---
+++ GUI.py
@@ -0,0 +1,29 @@
+import wx
+
+
+'''
+app = wx.App()
+
+frame = wx.Frame(None, wx.ID_ANY, "Fuck Ali Abdi")
+frame.Show()
+
+app.MainLoop()
+'''
+
+#djfhsjkdfhsaf
+
+import functions
+
+
+
+class MyFrame(wx.Frame):
+
+    """ We simply derive a new class of Frame. """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(600, 100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.Show(True)
+
+app = wx.App(False)
+frame = MyFrame(None, 'Small editor')
+app.MainLoop()
